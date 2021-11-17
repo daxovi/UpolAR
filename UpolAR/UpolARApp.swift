0713c40f6832ea7b5990d68f319f627e2c59e43e
daxovi <daxovi@gmail.com>
2021-11-17 21:03:54 +0100

    Initial Commit

---
+++ UpolAR/UpolARApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpolARApp.swift
+//  UpolAR
+//
+//  Created by Dalibor Janeček on 17.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct UpolARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
