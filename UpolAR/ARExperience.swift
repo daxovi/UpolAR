5e4bbbb46986350616e3d07b362a6692ffb32621
daxovi <daxovi@gmail.com>
2022-01-17 18:51:36 +0100

    first game

--- UpolAR/ARExperience.swift
+++ UpolAR/ARExperience.swift
@@ -6,77 +6,91 @@
 //
 
 import SwiftUI
-import SpriteKit
+import RealityKit
 
 struct ARExperience: View {
     @Environment(\.presentationMode) var presentationMode
     
-    @AppStorage("bestScore") var bestScore = 0
-    @StateObject private var gameScene = BreakoutGame()
+    @State var color: UIColor = .red
+    @State var position: Float? = nil
+    @State var lastPosition: Float = 0.0
+    @State var isPressedStart: Bool = false
     
     var body: some View {
-        ZStack {
-            SpriteView(scene: gameScene)
+        ZStack(alignment: .topLeading) {
+            GamesARView(position: $position, isPressedStart: $isPressedStart).ignoresSafeArea()
+            buttons
             
-            VStack(alignment: .leading) {
-                Text("Level: \(gameScene.level)")
-                    .font((.system(size: 12, weight: .heavy)))
-                    .foregroundColor(.white)
-                    .padding(.leading)
-                    .padding(.top, 42)
-                
-                Text("Score: \(gameScene.score)")
-                    .font((.system(size: 24, weight: .heavy)))
-                    .foregroundColor(.white)
-                    .padding(.leading)
-                Button {
-                    presentationMode.wrappedValue.dismiss()
-                } label: {
-                    Text("close")
-                }
-                .padding(.leading)
-                Spacer()
+            
+        }
+        .gesture(
+            DragGesture(minimumDistance: 0)
+                    .onChanged ({ (value) in
+                        if value.location.y > 280 {
+                            position = getPosition(value: value)
             }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            
-            if gameScene.isGameOver {
-                VStack {
-                    Text("Game Over")
-                        .font((.system(size: 42, weight: .heavy)))
-                        .foregroundColor(.white)
-                        .padding(.leading)
-                    
-                    if gameScene.score > bestScore {
-                        Text("New Best Score!!!")
-                            .font((.system(size:34, weight: .heavy)))
-                            .foregroundColor(.white)
-                            .padding(.leading)
                         
-                        Text("\(gameScene.score)")
-                            .font((.system(size:34, weight: .heavy)))
-                            .foregroundColor(.white)
-                            .padding(.leading)
-                        }
-                    
-                    Text("Play Again")
-                        .font((.system(size:24, weight: .heavy, design: .rounded)))
-                        .foregroundColor(.white)
-                        .padding(.leading)
-                        .padding()
-                        .onTapGesture {
-                            if gameScene.score > bestScore {
-                                bestScore = gameScene.score
-                            }
-                            
-                            gameScene.isGameOver.toggle()
-                            gameScene.makeBall()
-                            gameScene.makeBricks()
-                            gameScene.score = 0
-                        }
-                }
+                    })
+                    .onEnded({ _ in
+            lastPosition = position ?? 0.0
+        }))
+        
+    }
+    
+    // MARK: Functions
+    func getPosition(value: DragGesture.Value) -> Float {
+        // print("DEBUG: start: \(value.startLocation.x) current: \(value.location.x)")
+        
+        var distance = Float(value.location.x - value.startLocation.x)
+        let boundaries: [Float] = [Float(UIScreen.main.bounds.width) / -4,
+                                   Float(UIScreen.main.bounds.width) / 4]
+        
+        if distance < boundaries[0] {
+            distance = boundaries[0]
+        } else if distance > boundaries[1] {
+            distance = boundaries[1]
+        }
+        
+        distance = distance + boundaries[1]
+        
+        let xPercentage = Float(distance / (boundaries[1]*2))
+        // print("DEBUG: xPercentage: \(xPercentage)")
+        
+        let totalPosition = (xPercentage * 0.40) - 0.20 + lastPosition
+        
+        if totalPosition > 0.09 {
+            return 0.09
+        } else if totalPosition < -0.09 {
+            return -0.09
+        } else {
+            return totalPosition
+        }
+        
+    }
+}
+
+extension ARExperience {
+    // MARK: Buttons
+    var buttons: some View {
+        HStack {
+            Button {
+                collisionSubscribing = nil
+                presentationMode.wrappedValue.dismiss()
+            } label: {
+                Text("Exit")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.red)
+                    .padding()
+            }
+            
+            
+            Button {
+                isPressedStart = true
+            } label: {
+                Text("Start")
             }
         }
-        .ignoresSafeArea()
     }
 }
 
---
+++ UpolAR/GamesARView.swift
@@ -0,0 +1,234 @@
+//
+//  GamesARView.swift
+//  UpolAR
+//
+//  Created by Dalibor Janeček on 16.01.2022.
+//
+
+import SwiftUI
+import RealityKit
+import Combine
+
+var collisionSubscribing:Cancellable?
+
+struct GamesARView: UIViewRepresentable {
+    @Binding var position: Float?
+    @Binding var isPressedStart: Bool
+    let force: Float = 0.08
+    
+    // modely
+    var paddleModel = ModelEntity(mesh: MeshResource.generateBox(width: 0.1, height: 0.02, depth: 0.02, cornerRadius: 0, splitFaces: false),
+                                  materials: [SimpleMaterial(color: .green, roughness: 1, isMetallic: false)])
+    var ballModel = ModelEntity(mesh: MeshResource.generateSphere(radius: 0.01),
+                                materials: [SimpleMaterial(color: .red, roughness: 0.3, isMetallic: false)])
+    
+    // Physics body component
+    let kinematicBodyComponent = PhysicsBodyComponent(
+        massProperties: .default,
+        material: .generate(friction: 0, restitution: 0),
+        mode: .kinematic)
+    let brickBodyComponent = PhysicsBodyComponent(
+        massProperties: .default,
+        material: .generate(friction: 0, restitution: 1),
+        mode: .kinematic)
+    let dynamicBodyComponent = PhysicsBodyComponent(
+        massProperties: PhysicsMassProperties(mass: 0.01),
+        material: .generate(friction: 0, restitution: 0),
+        mode: .dynamic)
+    
+    // material
+    let occlusionMaterial = OcclusionMaterial()
+    let transparentMaterial = SimpleMaterial(color: .blue.withAlphaComponent(0.0), roughness: 1, isMetallic: false)
+    let simpleMaterial = SimpleMaterial(color: .blue, roughness: 1, isMetallic: false)
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        // generování anchor ze souboru v resources - nefunguje ovládání gravitace
+        /*
+        let arView = ARView(frame: .zero)
+        let anchor = AnchorEntity(.image(group: "AR Resources", name: "games"))
+        anchor.generateCollisionShapes(recursive: false)
+        arView.scene.addAnchor(anchor)
+         */
+        
+        let arView = ARView(frame: .zero)
+        // Load the "Breakout" scene from the "GameScene" Reality File
+        let anchor = try! GameScene.loadBreakout()
+        anchor.generateCollisionShapes(recursive: false)
+        
+        arView.scene.anchors.append(anchor)
+
+        // podlaha
+        let floor = MeshResource.generateBox(width: 0.297, height: 0.001, depth: 0.420)
+        let floorModel = ModelEntity(mesh: floor, materials: [occlusionMaterial])
+        floorModel.components[PhysicsBodyComponent.self] = kinematicBodyComponent
+        floorModel.generateCollisionShapes(recursive: false)
+        anchor.addChild(floorModel)
+        
+        // vrchní kryt
+        let roofModel = ModelEntity(mesh: floor, materials: [transparentMaterial])
+        roofModel.components[PhysicsBodyComponent.self] = kinematicBodyComponent
+        roofModel.generateCollisionShapes(recursive: false)
+        roofModel.position = [0, 0.02, 0]
+        anchor.addChild(roofModel)
+        
+        let top = MeshResource.generateBox(width: 0.297, height: 0.02, depth: 0.02)
+        let side = MeshResource.generateBox(width: 0.02, height: 0.02, depth: 0.420)
+        
+        let rightModel = ModelEntity(mesh: side, materials: [transparentMaterial])
+        rightModel.position = [0.1585, 0, 0]
+        rightModel.name = "rightModel"
+        let leftModel = ModelEntity(mesh: side, materials: [transparentMaterial])
+        leftModel.position = [-0.1585, 0, 0]
+        leftModel.name = "leftModel"
+        
+        let topModel = ModelEntity(mesh: top, materials: [transparentMaterial])
+        topModel.position = [0, 0, -0.22]
+        topModel.name = "topModel"
+        let bottomModel = ModelEntity(mesh: top, materials: [transparentMaterial])
+        bottomModel.position = [0, 0, 0.22]
+        bottomModel.name = "bottomModel"
+        
+        let sides:[ModelEntity] = [rightModel, leftModel, topModel, bottomModel]
+        for model in sides {
+            model.components[PhysicsBodyComponent.self] = kinematicBodyComponent
+            model.generateCollisionShapes(recursive: false)
+            anchor.addChild(model)
+        }
+        
+        // brick
+        for column in -1..<2 {
+            for row in 0..<6 {
+                let brick = MeshResource.generateBox(width: 0.08, height: 0.02, depth: 0.01, cornerRadius: 0, splitFaces: false)
+                let brickModel = ModelEntity(mesh: brick, materials: [simpleMaterial])
+                brickModel.components[PhysicsBodyComponent.self] = brickBodyComponent
+                brickModel.generateCollisionShapes(recursive: false)
+                brickModel.position = [Float(column) * 0.09, 0, -0.075 - Float(row) * Float(0.02)]
+                brickModel.name = "brick"
+                anchor.addChild(brickModel)
+            }
+        }
+        
+        /*
+        let brick = MeshResource.generateBox(width: 0.08, height: 0.02, depth: 0.01, cornerRadius: 0, splitFaces: false)
+        let brickModel = ModelEntity(mesh: brick, materials: [simpleMaterial])
+        brickModel.components[PhysicsBodyComponent.self] = brickBodyComponent
+        brickModel.generateCollisionShapes(recursive: false)
+        brickModel.position = [0, 0, -0.075]
+        anchor.addChild(brickModel)
+         */
+        
+                
+        // kinematic = odrazí ostatní ale nepohne se
+        // dynamic = odrazí a pohne se
+        paddleModel.components[PhysicsBodyComponent.self] = kinematicBodyComponent
+        paddleModel.generateCollisionShapes(recursive: false)
+        
+        ballModel.components[PhysicsBodyComponent.self] = dynamicBodyComponent
+        ballModel.generateCollisionShapes(recursive: false)
+        
+        ballModel.name = "ballModel"
+        ballModel.isEnabled = false
+        paddleModel.name = "paddleModel"
+        
+        paddleModel.position = [0, 0, 0.18]
+        anchor.addChild(paddleModel)
+        anchor.addChild(ballModel)
+        
+                
+        collisionSubscribing = arView.scene.subscribe(to: CollisionEvents.Began.self) { event in
+            ballModel.clearForcesAndTorques()
+
+            if event.entityA == ballModel, event.entityB == leftModel {
+                print("left!")
+                ballModel.addForce([force,0,0], relativeTo: ballModel)
+            }
+            
+            if event.entityA == ballModel, event.entityB == rightModel {
+                print("right!")
+                ballModel.addForce([-1*(force),0,0], relativeTo: ballModel)
+            }
+            
+            if event.entityA == ballModel, event.entityB == topModel {
+                print("top!")
+                ballModel.addForce([0,0,force], relativeTo: ballModel)
+            }
+            
+            if event.entityA == ballModel, event.entityB == paddleModel {
+                print("paddle!")
+                ballModel.addForce([0,0,-1*(force)], relativeTo: ballModel)
+            }
+            
+            if event.entityA == ballModel, event.entityB == bottomModel {
+                print("bottom!")
+                DispatchQueue.main.async {
+                    ballModel.isEnabled = false
+                    ballModel.position = [0, 0, 0]
+                }
+            }
+            
+            if event.entityA == ballModel, event.entityB.name == "brick" {
+                print("brick!")
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: {
+                    event.entityB.isEnabled = false
+                })
+            }
+            
+            /*
+            if event.entityA == puckModel, event.entityB == brickModel {
+                print("HIT!")
+                hit = "HIT"
+                puckModel.addForce([5,0,0], relativeTo: puckModel)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                    sceneElement.removeChild(brickModel)
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                sceneElement.addChild(brickModel)
+                }
+            }
+            
+            if event.entityA == puckModel, event.entityB == newBrick {
+                print("HIT!")
+                hit = "HIT"
+                puckModel.addForce([5,0,0], relativeTo: puckModel)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                    sceneElement.removeChild(newBrick)
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                sceneElement.addChild(newBrick)
+                }
+            }
+             */
+        }
+        
+        
+        
+        return arView
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {
+        // pohyb padle
+        if let entityPosition = self.position {
+            paddleModel.position = [entityPosition, 0, 0.18]
+            //uiView.scene.anchors[0].addChild(paddleModel)
+            
+            /*
+            DispatchQueue.main.async {
+                position = nil
+            }
+             */
+        }
+        
+        // start
+        if isPressedStart {
+            ballModel.isEnabled = true
+            ballModel.position = [0, 0, 0]
+            ballModel.addForce([force,0,force], relativeTo: ballModel)
+            // uiView.scene.anchors[0].addChild(ballModel.clone(recursive: true))
+            
+            DispatchQueue.main.async {
+                isPressedStart = false
+            }
+        }
+    }
+}
