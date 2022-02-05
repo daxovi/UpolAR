138c3ac85b916d6a26654e9dd39441ae95ad245e
daxovi <daxovi@gmail.com>
2022-02-05 22:29:09 +0100

    videomaterial

--- UpolAR/ARExperience.swift
+++ UpolAR/ARExperience.swift
@@ -85,7 +85,6 @@ extension ARExperience {
                                 .cornerRadius(10)
                         )
                         .padding()
-                    
                 }
                 
                 Spacer()
